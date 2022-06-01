2185071005a847c48cc736a028f1fd960a7bb662
João Vitor <vitorjoao.jv626@gmail.com>
2022-06-01 19:53:39 -0300

    Arrumando o scroll e botoões flutuantes

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,15 @@ function subirTela() {
     behavior: 'smooth'
   });
 }
+
+function decidirBotaoScroll() {
+  if(window.scrollY === 0) {
+    //ocultar botão
+    document.querySelector('.scrollbutton').style.display = 'none';
+  } else {
+    //mostrar botão
+    document.querySelector('.scrollbutton').style.display = 'block';
+  }
+}
+
+window.addEventListener('scroll', decidirBotaoScroll);
